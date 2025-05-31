540e0e4da595ed79179bfcccc5ae80d5ccf04bb2
yrc <958480638@qq.com>
2025-05-31 22:08:57 +0800

    build添加子模块

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "2.1.0"
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
 }
@@ -21,6 +21,7 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation(project(":modules:v2ray-core-wrapper"))
 }
 
 compose.desktop {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,5 @@ pluginManagement {
     }
 }
 
+include(":modules:v2ray-core-wrapper")
 rootProject.name = "v2rayk"
